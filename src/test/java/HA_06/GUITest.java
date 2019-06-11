998944e92e247aaefbbf69ae7822ee405d26e884
Xavier Ngansop <ngaxavi@gmail.com>
2019-06-11 11:52:03 +0200

    HA06_Corrected

--- src/test/java/HA_06/GUITest.java
+++ src/test/java/HA_06/GUITest.java
@@ -18,7 +18,40 @@ import static org.junit.Assert.assertEquals;
  * Homework No 06
  * University of Kassel
  */
-
+// HA06: 08/10
+// -2 java.util.NoSuchElementException
+//	at java.util.Optional.orElseThrow(Optional.java:290)
+//	at org.testfx.service.finder.impl.WindowFinderImpl.window(WindowFinderImpl.java:70)
+//	at org.testfx.robot.impl.WriteRobotImpl.fetchTargetWindow(WriteRobotImpl.java:86)
+//	at org.testfx.robot.impl.WriteRobotImpl.write(WriteRobotImpl.java:78)
+//	at org.testfx.robot.impl.WriteRobotImpl.write(WriteRobotImpl.java:73)
+//	at org.testfx.api.FxRobot.write(FxRobot.java:506)
+//	at org.testfx.api.FxRobot.write(FxRobot.java:60)
+//	at HA_06.GUITest.commandPatternTest(GUITest.java:61)
+//	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+//	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+//	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+//	at java.lang.reflect.Method.invoke(Method.java:498)
+//	at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:50)
+//	at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+//	at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:47)
+//	at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+//	at org.junit.internal.runners.statements.RunBefores.evaluate(RunBefores.java:26)
+//	at org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)
+//	at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:325)
+//	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:78)
+//	at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:57)
+//	at org.junit.runners.ParentRunner$3.run(ParentRunner.java:290)
+//	at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:71)
+//	at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:288)
+//	at org.junit.runners.ParentRunner.access$000(ParentRunner.java:58)
+//	at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:268)
+//	at org.junit.runners.ParentRunner.run(ParentRunner.java:363)
+//	at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
+//	at com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:68)
+//	at com.intellij.rt.execution.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:47)
+//	at com.intellij.rt.execution.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:242)
+//	at com.intellij.rt.execution.junit.JUnitStarter.main(JUnitStarter.java:70)
 public class GUITest extends ApplicationTest {
 
     private Controller controller;
