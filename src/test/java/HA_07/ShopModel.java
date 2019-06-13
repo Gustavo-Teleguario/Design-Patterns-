20a344adf037e5303730d8697aaf448a1ec64cb7
Gustavo Teleguario <Maynor_Teleguario@hotmail.com>
2019-06-14 01:19:01 +0200

    Shop & WareHouse Model update

--- src/test/java/HA_07/ShopModel.java
+++ src/test/java/HA_07/ShopModel.java
@@ -1,4 +1,39 @@
 package HA_07;
 
-public class ShopModel {
+import de.uniks.networkparser.graph.Cardinality;
+import de.uniks.networkparser.graph.Clazz;
+import de.uniks.networkparser.graph.DataType;
+import org.junit.Test;
+import org.sdmlib.models.classes.ClassModel;
+
+public class ShopModel
+{
+    @Test
+    public void ShopModel(){
+
+        ClassModel model = new ClassModel("Shop");
+
+        Clazz shopClass = model.createClazz("Shop");
+
+        Clazz shopCustomerClass = model.createClazz("ShopCustomer")
+                .withAttribute("addresse", DataType.STRING)
+                .withAttribute("name", DataType.STRING);
+
+        Clazz shopProductClass = model.createClazz("ShopProduct")
+                .withAttribute("id", DataType.STRING)
+                .withAttribute("inStock",DataType.DOUBLE)
+                .withAttribute("name", DataType.STRING)
+                .withAttribute("price",DataType.DOUBLE);
+
+        Clazz shopOrderClass = model.createClazz("ShopOrder")
+                .withAttribute("id",DataType.STRING);
+
+        shopClass.withBidirectional(shopProductClass,"products", Cardinality.MANY,"shop", Cardinality.ONE);
+        shopClass.withBidirectional(shopCustomerClass, "customers",Cardinality.MANY,"shop",Cardinality.ONE);
+        shopClass.withBidirectional(shopOrderClass,"orders",Cardinality.MANY,"shop",Cardinality.ONE);
+        shopCustomerClass.withBidirectional(shopOrderClass,"orders", Cardinality.MANY,"shopCustomer", Cardinality.ONE);
+        shopOrderClass.withBidirectional(shopProductClass,"products",Cardinality.MANY,"orders",Cardinality.MANY);
+
+        model.generate("src/main/java/HA_07");
+    }
 }
--- src/test/java/HA_07/WareHouseModel.java
+++ src/test/java/HA_07/WareHouseModel.java
@@ -1,4 +1,51 @@
 package HA_07;
 
+import de.uniks.networkparser.graph.Cardinality;
+import de.uniks.networkparser.graph.Clazz;
+import de.uniks.networkparser.graph.DataType;
+import org.junit.Test;
+import org.sdmlib.models.classes.ClassModel;
+
 public class WareHouseModel {
+
+    @Test
+    public void wareHouseModel() {
+
+        ClassModel model = new ClassModel("WareHouse");
+
+        Clazz lotClass = model.createClazz("Lot")
+                .withAttribute("id", DataType.STRING)
+                .withAttribute("lotSize", DataType.DOUBLE);
+
+        Clazz palettePlaceClass = model.createClazz("PalettePlace")
+                .withAttribute("column", DataType.DOUBLE)
+                .withAttribute("id", DataType.STRING)
+                .withAttribute("row", DataType.DOUBLE);
+
+        Clazz wareHouseClass = model.createClazz("WareHouse")
+                .withAttribute("name", DataType.STRING);
+
+        Clazz wareHouseOrderClass = model.createClazz("WareHouseOrder")
+                .withAttribute("addresse", DataType.STRING)
+                .withAttribute("id", DataType.STRING)
+                .withAttribute("product", DataType.STRING);
+
+        Clazz wareHouseProductClass = model.createClazz("WareHouseProduct")
+                .withAttribute("name", DataType.STRING)
+                .withAttribute("id", DataType.STRING);
+
+        wareHouseClass.withBidirectional(wareHouseClass, "orders", Cardinality.MANY, "wareHouse", Cardinality.ONE);
+        wareHouseClass.withBidirectional(wareHouseProductClass, "products", Cardinality.MANY, "wareHouse", Cardinality.ONE);
+        wareHouseClass.withBidirectional(palettePlaceClass, "places", Cardinality.MANY, "wareHouse", Cardinality.ONE);
+
+        wareHouseOrderClass.withBidirectional(wareHouseProductClass, "products", Cardinality.MANY, "orders", Cardinality.MANY);
+
+        wareHouseProductClass.withBidirectional(lotClass, "lots", Cardinality.MANY, "wareHouseProduct", Cardinality.ONE);
+
+        lotClass.withBidirectional(palettePlaceClass, "palettePlace", Cardinality.ONE, "lot", Cardinality.ONE);
+
+        model.generate("src/main/java");
+
+    }
+
 }
