6903d8bdd5875b3af53401e21bf5cbb789955173
Gustavo Teleguario <Maynor_Teleguario@hotmail.com>
2019-07-05 12:46:50 +0200

    Hausaufgabe_10

--- src/test/java/HA_10/ComposeContainerTest.java
+++ src/test/java/HA_10/ComposeContainerTest.java
@@ -1,19 +1,19 @@
-package HA_09;
+package HA_10;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.testcontainers.containers.DockerComposeContainer;
-import org.testcontainers.shaded.com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import java.io.File;
+
 /**
  * Created by Maynor Teleguario
  * 30247228
  * Design Patterns Summer Semester 2019
- * Homework No 09
+ * Homework No 10
  * University of Kassel
  */
-public class DockerComposeTest {
+public class ComposeContainerTest {
 
    @Test
    public void testContainer(){
@@ -27,13 +27,11 @@ public class DockerComposeTest {
                .withExposedService("shopserver", 5001)
                .withExposedService("warehouseserver", 5002)
                .withLocalCompose(true);
-       File composeFile = new File("src/main/java/HA_09/docker-compose.yml");
-       File scriptFile = new File("src/main/java/HA_09/docker.sh");
-       File shopDockerfile = new File("src/main/java/HA_09/DockerShop/Dockerfile");
-       File warehoouseDockerfile = new File("src/main/java/HA_09/DockerWarehouse");
+       File composeFile = new File("src/main/java/HA_10/docker-compose.yml");
+       File shopDockerfile = new File("src/main/java/HA_10/DockerShop/Dockerfile");
+       File warehoouseDockerfile = new File("src/main/java/HA_10/DockerWarehouse/Dockerfile");
        Assert.assertTrue(shopDockerfile.exists());
        Assert.assertTrue(warehoouseDockerfile.exists());
-       Assert.assertTrue(scriptFile.exists());
        Assert.assertTrue(composeFile.exists());
 
        System.out.println("******************** ***************************");
